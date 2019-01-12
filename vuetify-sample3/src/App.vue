7cd4c2251c7fff7611603151758071c984a8af55
red_army <cwj5e21@gmail.com>
2019-01-12 17:09:31 +0900

    change template

---
+++ vuetify-sample3/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Template/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Template from './components/Template'
+
+export default {
+  name: 'App',
+  components: {
+    Template
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ vuetify-sample3/src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <v-toolbar
+      fixed
+      color="teal lighten-3"
+      dark
+      scroll-off-screen
+      scroll-target="#app"
+    >
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>more_vert</v-icon>
+      </v-btn>
+    </v-toolbar>
+</template>
